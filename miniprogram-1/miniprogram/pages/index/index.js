b2a6c10b26a834236933ae55152619381c33187d
1808268179 <96681476+1808268179@users.noreply.github.com>
2025-06-30 18:12:38 +0800

    Add files via upload

--- miniprogram-1/miniprogram/pages/index/index.js
+++ miniprogram-1/miniprogram/pages/index/index.js
@@ -55,41 +55,68 @@ Page({
     this.setData({ isLoading: true });
     wx.showLoading({ title: 'AI正在努力识别中...' });
 
-    wx.uploadFile({
-      url: 'http://61.136.101.57:5000/predict',
-      filePath: this.data.imageSrc,
-      name: 'file',
-      success: (res) => {
-        const data = JSON.parse(res.data);
-        if (data.message === '成功') {
-          const topResult = data.top5_info[0];
-          this.setData({
-            result: {
-              name: topResult.name,
-              latin: '',
-              confidence: topResult.confidence,
-              features: []
+    // 1. 获取文件扩展名
+    const filePath = this.data.imageSrc;
+    const cloudPath = `recognition/${Date.now()}-${Math.floor(Math.random(0, 1) * 1000)}${filePath.match(/\.[^.]+?$/)[0]}`;
+
+    // 2. 将图片上传到云存储
+    wx.cloud.uploadFile({
+      cloudPath: cloudPath,
+      filePath: filePath,
+      success: res => {
+        // 图片上传成功，获取 fileID
+        const fileID = res.fileID;
+
+        // 3. 调用云函数进行识别
+        wx.cloud.callFunction({
+          name: 'recognition',
+          data: {
+            fileID: fileID
+          },
+          success: cfRes => {
+            // cfRes.result 是云函数返回的对象
+            if (cfRes.result.success) {
+                const data = cfRes.result.data;
+                const topResult = data.top5_info[0];
+                this.setData({
+                  result: {
+                    name: topResult.name,
+                    latin: '',
+                    confidence: topResult.confidence,
+                    features: []
+                  }
+                });
+                wx.showToast({
+                  title: '识别完成！',
+                  icon: 'success',
+                  duration: 2000
+                });
+            } else {
+                 // 云函数处理失败
+                 wx.showToast({
+                    title: '识别服务出错',
+                    icon: 'error'
+                  });
             }
-          });
-          wx.showToast({
-            title: '识别完成！',
-            icon: 'success',
-            duration: 2000
-          });
-        } else {
-          wx.showToast({
-            title: '识别失败：' + data.error,
-            icon: 'error'
-          });
-        }
-      },
-      fail: (err) => {
-        wx.showToast({
-          title: '请求失败：' + err.errMsg,
-          icon: 'error'
+          },
+          fail: err => {
+            wx.showToast({
+                title: '云函数调用失败',
+                icon: 'error'
+              });
+          },
+          complete: () => {
+              this.setData({ isLoading: false });
+              wx.hideLoading();
+          }
         });
       },
-      complete: () => {
+      fail: e => {
+        // 图片上传到云存储失败
+        wx.showToast({
+          title: '图片上传失败',
+          icon: 'error'
+        });
         this.setData({ isLoading: false });
         wx.hideLoading();
       }
